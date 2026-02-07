c65339e36a19f0e2af64ebac977ffeb91ca21ddc
kimheang <skimheang63@gamil.com>
2026-02-07 14:48:08 +0700

    change

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+                    console.log(`Server running on port ${PORT}`);
 });
 
